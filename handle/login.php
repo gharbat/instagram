83dab3216eb894dce9711e477d1ca42810646ab4
gharbat <Ar0799298231*>
2019-11-08 15:26:14 +0200

    register page - front end

--- handle/login.php
+++ handle/login.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: arbio
- * Date: 10/25/2019
- * Time: 9:43 AM
- */
+include_once ("../include/config.php");
+
+if($_SERVER['REQUEST_METHOD']==="POST"){
+    if(!empty($_POST['email']) && isset($_POST['email'])){
+        $email = $_POST['email'];
+    }else{
+        header("location: login.php");
+
+    }
+
+    if(!empty($_POST['password']) && isset($_POST['password'])){
+        $password = $_POST['password'];
+    }else{
+        header("location: login.php");
+    }
+
+
+
+
+
+}
